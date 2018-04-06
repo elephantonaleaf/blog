6194e4462261adc9490c96b213d9cee3a3322f21
Harrison Kim <namo918@gmail.com>
2018-04-06 09:22:16 +0800

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406011312) do
+ActiveRecord::Schema.define(version: 20180406011632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
